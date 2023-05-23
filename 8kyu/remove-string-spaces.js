205061da759775d6ff7c925fb63ff95f8ee91351
peter_olsen <peterxavierolsen@gmail.com>
2023-05-23 14:31:44 -0400

    Completed remove string spaces challenge

---
+++ 8kyu/remove-string-spaces.js
@@ -0,0 +1,13 @@
+// INSTRUCTIONS:
+// Write a function that removes the spaces from the string, then return the resultant string.
+
+function noSpace(str) {
+    return str.replace(/\s/g,"");
+}
+
+console.log(noSpace("8 j 8   mBliB8g  imjB8B8  jl  B"));
+// --> "8j8mBliB8gimjB8B8jlB"
+console.log(noSpace("8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd"));
+// --> "88Bifk8hB8BB8BBBB888chl8BhBfd"
+console.log(noSpace("8aaaaa dddd r     "));
+// --> "8aaaaaddddr"
